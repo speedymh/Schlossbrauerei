e4a517b1b3db2795e74cfbfea047a9dd6de74f29
speedymh <matthias_hoppe@outlook.de>
2017-09-08 14:27:56 +0200

    Code von Separiert-Ordner in Haupt-Ordner eingefügt

--- slider.js
+++ slider.js
@@ -1,23 +1,71 @@
-  $(function() {
-  var i = 0;
-  slideCount = 5;
-  function timeout() {
-    setTimeout(function() {
-      // Move $("#quote" + i) off to the left
-      $("#quote" + i).animate({
-        right: "120%"
-      }, 2000);
-      // Change selected quote
-      i++;
-      if(i > (slideCount - 1)) {
-        i = 0;
-      }
-      // Move $("#quote" + i) to right side then back to middle
-      $("#quote" + i).css("right", "-60%");
-      $("#quote" + i).animate({
-        right: "20%"
-      }, 2000);
-      timeout();
-    }, 7000);
-  };
-  timeout();
+$(document).ready(function () {
+    //rotation speed and timer
+    var speed = 2000;
+
+    var run = setInterval(rotate, speed);
+    var slides = $('.slide');
+    var container = $('#slides ul');
+    var elm = container.find(':first-child').prop("tagName");
+    var item_width = container.width();
+    var previous = 'prev'; //id of previous button
+    var next = 'next'; //id of next button
+    slides.width(item_width); //set the slides to the correct pixel width
+    container.parent().width(item_width);
+    container.width(slides.length * item_width); //set the slides container to the correct total width
+    container.find(elm + ':first').before(container.find(elm + ':last'));
+    resetSlides();
+
+
+    //if user clicked on prev button
+
+    $('#buttons a').click(function (e) {
+        //slide the item
+
+        if (container.is(':animated')) {
+            return false;
+        }
+        if (e.target.id == previous) {
+            container.stop().animate({
+                'left': 0
+            }, 1500, function () {
+                container.find(elm + ':first').before(container.find(elm + ':last'));
+                resetSlides();
+            });
+        }
+
+        if (e.target.id == next) {
+            container.stop().animate({
+                'left': item_width * -2
+            }, 1500, function () {
+                container.find(elm + ':last').after(container.find(elm + ':first'));
+                resetSlides();
+            });
+        }
+
+        //cancel the link behavior
+        return false;
+
+    });
+
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    container.parent().mouseenter(function () {
+        clearInterval(run);
+    }).mouseleave(function () {
+        run = setInterval(rotate, speed);
+    });
+
+
+    function resetSlides() {
+        //and adjust the container so current is in the frame
+        container.css({
+            'left': -1 * item_width
+        });
+    }
+
+});
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+function rotate() {
+    $('#next').click();
+}
